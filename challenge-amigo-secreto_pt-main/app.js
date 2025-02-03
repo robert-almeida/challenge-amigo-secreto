f35e1bbb7e4c7e22d3887892464ba2f2a3095736
Robert Almeida <robertwagner.almeida@outlook.com>
2025-02-02 23:16:34 -0300

    Amigo Secreto v1

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,45 @@
 let amigos = []
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    } else {
+        amigos.push(nome)
+    }
+    console.log(amigos)
+    atualizarLista();
+    nome = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((nome) => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo(){
+    
+    if (amigos.length === 0){
+        alert ("Introduzir amigo");
+        return;
+    }
+    
+    let mensagem = document.getElementById("mensagem");
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+
+    //Oculta lista de amigos
+    let lista = document.getElementById("listaAmigos");
+    lista.style.display = "none";
+
+    //Mostra amigo sorteado
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> Amigo Sorteado: ${amigoSorteado}</li>`;
+}
